ad9f361239ff22f5d57ebfd9b0f9706939ea50d3
adambita <adafame@gmail.com>
2019-05-07 10:30:01 +0100

    mise en place du twig

---
+++ auteur.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ .'/views');
+$twig = new \Twig\Environment($loader, [
+'cache' => false, // __DIR__ .'/var'
+]);
+
+echo $twig->render('auteur.html.twig');
+
+?>
