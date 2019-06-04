7cb5426b9d43a930f72eeeadf9b929363f20eb3a
adambita <adafame@gmail.com>
2019-06-04 07:41:43 +0100

    Mise a jour du fichier index.php, le rendant ainsi dynamique comme les autres

--- public/index.php
+++ public/index.php
@@ -1,12 +1,15 @@
 <?php
-
+require_once ('../src/Modele/chapitrebd.php');
 require_once '../vendor/autoload.php';
+$chapitres= findAll($bdd);
+/* var_dump($chapitres);
+die(); */
 
 $loader = new \Twig\Loader\FilesystemLoader('../views');
 $twig = new \Twig\Environment($loader, [
 'cache' => false, // __DIR__ .'/var'
 ]);
 
-echo $twig->render('home.html.twig');
+echo $twig->render('home.html.twig',['chapitres'=>$chapitres]);
 
 ?>
